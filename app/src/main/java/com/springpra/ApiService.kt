273ac80b734d99b484a904f0d20515c9e5f542f2
Kingjha13 <avnishjha277@gmail.com>
2026-09-19 18:16:40 +0530

    New Api

--- app/src/main/java/com/springpra/ApiService.kt
+++ app/src/main/java/com/springpra/ApiService.kt
@@ -1,16 +1,16 @@
 package com.springpra
 
-import retrofit2.Response
 import retrofit2.http.Body
-import retrofit2.http.DELETE
 import retrofit2.http.GET
-import retrofit2.http.HTTP
-import retrofit2.http.Header
 import retrofit2.http.POST
-import retrofit2.http.PUT
-import retrofit2.http.Query
 
 interface ApiService {
     @POST("/registerusername")
     suspend fun createUser(@Body ree : RegisterUser) : Boolean
+    @POST("/signin")
+    suspend fun logg(@Body login : Login) : SignInResponse
+    @POST("/verify")
+    suspend fun ver(@Body verify: String?) : String
+    @POST("/getname")
+    suspend fun getName(@Body token: String?) : String
 }
